d64e35180af0ba6d4def43638362f0032f71e075
Willian Antunes <willian.lima.antunes@gmail.com>
2021-07-17 19:12:25 -0300

    chore: add core functionality to extract episode details

---
+++ pyfriends/core.py
@@ -0,0 +1,111 @@
+import re
+
+from dataclasses import dataclass
+from dataclasses import field
+from enum import Enum
+from pathlib import Path
+from typing import Generator
+from typing import List
+from typing import Optional
+
+from bs4 import BeautifulSoup
+from bs4 import Tag
+
+where_the_files_are = "../seasons"
+folder_seasons = Path(where_the_files_are)
+friends = ["Chandler", "Joey", "Monica", "Phoebe", "Rachel", "Ross"]
+seasons = ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"]
+
+regex_episode_number = re.compile(r"^\d{2}(\d{2})(-\d{2}(\d{2}))?$", re.IGNORECASE)
+regex_scene_details = re.compile(r"^\[Scene: (.+)\]$", re.IGNORECASE)
+regex_transcription_line = re.compile(r"(\w+): ?(.+)", re.IGNORECASE)
+
+
+class SceneCategory(Enum):
+    OPENING = "before opening"
+    MAIN = "main"
+    CLOSING_CREDITS = "after closing credits"
+
+
+@dataclass(frozen=True)
+class Transcription:
+    character: str
+    line: str
+
+
+@dataclass
+class Scene:
+    category: SceneCategory
+    description: Optional[str] = None
+    transcriptions: List[Transcription] = field(default_factory=list)
+
+
+@dataclass(frozen=True)
+class Episode:
+    number: str
+    title: str
+    scenes: List[Scene] = field(default_factory=list)
+
+
+def retrieve_episode_details(season: int, episode: Optional[int] = None) -> Generator[Episode, None, None]:
+    # Configure glob pattern
+    season_number = str(season).rjust(2, "0")
+    episode_number = str(episode).rjust(2, "0") if episode else None
+    glob_pattern = f"{season_number}*.html" if not episode_number else f"{season_number}{episode_number}*.html"
+
+    for episode_path in folder_seasons.glob(glob_pattern):
+        with open(episode_path.absolute(), mode="r", encoding="iso-8859-1") as episode_file:
+            soup = BeautifulSoup(episode_file, "html.parser")
+        # Episode's metadata
+        match = regex_episode_number.match(episode_path.stem)
+        if not match:
+            continue
+        episodes_groups = match.groups()
+        number_1, _, number_2 = episodes_groups
+        episode_number = number_1 if number_2 is None else f"{number_1}-{number_2}"
+        title = soup.find("title").text
+        episode = Episode(episode_number, title)
+        # Let's get all transcription and extract what we need
+        scene = Scene(SceneCategory.OPENING)
+        scene_category = SceneCategory.OPENING
+        all_transcriptions = soup.find_all("p")
+        for index, transcription_line in enumerate(all_transcriptions):
+            text = transcription_line.text.replace("\n", " ")
+            if not text:
+                continue
+            # As text has content, we can do what we want 👀
+            # Basic stuff to define the scene 🎬
+            for previous_element in transcription_line.previous_elements:
+                if isinstance(previous_element, Tag):
+                    text_from_previous = previous_element.text.lower()
+                    after_opening = text_from_previous.startswith("opening credits")
+                    if after_opening:
+                        scene_category = SceneCategory.MAIN
+                    after_closing = text_from_previous.startswith("closing credits")
+                    if after_closing:
+                        scene_category = SceneCategory.CLOSING_CREDITS
+                    break
+            must_create_new_scene = scene.category != scene_category
+            if must_create_new_scene:
+                episode.scenes.append(scene)
+                scene = Scene(scene_category)
+            scene_details_line = regex_scene_details.match(text)
+            if scene_details_line:
+                description = scene_details_line.groups()[0]
+                if not scene.description:
+                    scene.description = description
+                else:
+                    episode.scenes.append(scene)
+                    scene = Scene(scene_category, description)
+                continue
+            # If the code is running here, then it will fill up the scene 🎞
+            match_character_line = regex_transcription_line.match(text)
+            if match_character_line:
+                character = match_character_line.groups()[0]
+                phrase = match_character_line.groups()[1]
+                transcription = Transcription(character.capitalize(), phrase)
+                scene.transcriptions.append(transcription)
+        # As the last scene is not appended
+        episode.scenes.append(scene)
+        # The entire defined episode
+        yield episode
---
+++ tests/test_core.py
@@ -0,0 +1,46 @@
+from unittest import TestCase
+
+from pyfriends.core import SceneCategory
+from pyfriends.core import retrieve_episode_details
+
+
+class Test(TestCase):
+    def test_retrieve_episode_details_from_0107(self):
+        # Arrange
+        season = 1
+        episode_number = 7
+        # Act
+        episodes = list(retrieve_episode_details(season, episode_number))
+        # Assert
+        self.assertEqual(1, len(episodes))
+        episode = episodes[0]
+        self.assertEqual(episode_number, int(episode.number))
+        self.assertEqual("The One With the Blackout", episode.title)
+        total_scenes = 21
+        self.assertEqual(total_scenes, len(episode.scenes))
+        scenes_before_opening = [scene for scene in episode.scenes if scene.category == SceneCategory.OPENING]
+        scenes_main = [scene for scene in episode.scenes if scene.category == SceneCategory.MAIN]
+        scenes_after_main = [scene for scene in episode.scenes if scene.category == SceneCategory.CLOSING_CREDITS]
+        self.assertTrue(total_scenes == (len(scenes_before_opening) + len(scenes_main) + len(scenes_after_main)))
+        # Evaluating before OPENING
+        self.assertEqual(2, len(scenes_before_opening))
+        before_opening_scene_1 = scenes_before_opening[0]
+        self.assertEqual(
+            "Central Perk, Rachel is introducing Phoebe, who is playing her guitar for the crowd.",
+            before_opening_scene_1.description,
+        )
+        self.assertEqual(2, len(before_opening_scene_1.transcriptions))
+        before_opening_scene_2 = scenes_before_opening[1]
+        self.assertEqual(
+            "The ATM vestibule of a bank, Chandler is inside. The lights go out, and he realizes he is trapped inside.",
+            before_opening_scene_2.description,
+        )
+        self.assertEqual(1, len(before_opening_scene_2.transcriptions))
+        # Evaluating after OPENING (which is category MAIN)
+        self.assertEqual(18, len(scenes_main))
+        # Evaluating after CLOSING CREDITS
+        self.assertEqual(1, len(scenes_after_main))
+        last_scene = scenes_after_main[0]
+        self.assertEqual(4, len(last_scene.transcriptions))
+        self.assertEqual(2, len(list(filter(lambda t: t.character == "Jill", last_scene.transcriptions))))
+        self.assertEqual(2, len(list(filter(lambda t: t.character == "Chandler", last_scene.transcriptions))))
